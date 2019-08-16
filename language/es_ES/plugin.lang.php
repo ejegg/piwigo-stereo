3081f6d38a5ae624dfe7331f8aa2bd3fda5dd220
Elliott Eggleston <ejegg@ejegg.com>
2019-08-15 19:02:33 -0500

    Add Spanish translation
    Mas o menos

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['STEREO_ADJUSTMENT'] = 'Ajustes de estereo';
+$lang['STEREO_INSTRUCTION'] = 'Utilize las teclas H, J, K, y L para alinear las imágenes. La tecla Shift amplia los ajustes.';
+$lang['STEREO_BATCH_INSTRUCTION'] = 'Establece alineación para todas las imágenes. Se puede alinear en una o en ambas orientaciones.';
+$lang['STEREO_EDIT_SUCCESS'] = 'Alineación actualizada con éxito';
+$lang['STEREO_BATCH_NO_INPUT'] = 'Entre alineación en al menos una orientación';
+$lang['STEREO_FORMAT'] = 'Formato de estereo:';
+$lang['STEREO_FORMAT_GIF'] = 'gif que menea';
+$lang['STEREO_FORMAT_CROSS_EYED'] = 'bizco';
+$lang['STEREO_FORMAT_WALL_EYED'] = 'estrábico';
+$lang['3D_FILTER'] = 'Imágenes 3-D';
